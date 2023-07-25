77a039757a98e25245dd3197c3784b2ae1399d35
Denis <bronzovdenis@bk.ru>
2023-07-25 22:34:47 +0300

    FM-19: add render logic random equations, fix EquationBlock component

--- src/components/EquationBlock/EquationBlock.tsx
+++ src/components/EquationBlock/EquationBlock.tsx
@@ -1,9 +1,23 @@
 import Image from 'next/image'
+import LongArrowLeft from '@/assets/images/LongArrowLeft.svg'
+import LongArrowRight from '@/assets/images/LongArrowRight.svg'
+import Button from '@/ui/Button/Button'
+import style from './EquationBlock.module.scss'
+import Equation from './components/Equation/Equation'
 
 const EquationBlock = () => {
     return (
-        <div>
-            equation
+        <div className={style['equation-wrapper']}>
+            <div className={style.equation}>
+                <Button variant='styleless'>
+                    <Image src={LongArrowLeft} alt='' />
+                </Button>
+                <Equation />
+                <Button variant='styleless'>
+                    <Image src={LongArrowRight} alt='' />
+                </Button>
+            </div>
+            <Button variant='styleless' text='Check' />
         </div>
     )
 }
