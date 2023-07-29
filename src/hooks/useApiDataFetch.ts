7cc91086d62f588de68faed542ed76aab4e2998c
Denis <bronzovdenis@bk.ru>
2023-07-29 14:42:42 +0300

    FM-23: fix api fetching logic

--- src/hooks/useApiDataFetch.ts
+++ src/hooks/useApiDataFetch.ts
@@ -1,9 +1,50 @@
-import { useEffect, useState } from 'react'
+import { useCallback } from 'react'
+import useSWRImmutable from 'swr/immutable'
 import { fetchRandomExploreStuff } from '@/api/fetchRandomExploreStuff'
-import useSWR from 'swr'
+
+const INITIAL_QUOTE = {
+    id: 1,
+    quote: 'Mathematics is the music of reason.',
+    author: 'James Joseph Sylvester',
+}
+
+const INITIAL_FACT = {
+    id: 1,
+    fact: 'The hour and minute hand of a clock coincide 22 times in a day.',
+}
 
 export const useApiDataFetch = <T>(url: string): T => {
-    const { data = {} as T, error } = useSWR<T>(url, fetchRandomExploreStuff)
+    let {
+        data = {} as T,
+        isLoading,
+        error,
+    } = useSWRImmutable(url, fetchRandomExploreStuff<T>)
+
+    const toEachRoute = useCallback(
+        (obj1: any, obj2: any) => {
+            if (/\/quote/.test(url)) data = obj1
+            else if (/\/fact/.test(url)) data = obj2
+        },
+        [isLoading, error]
+    )
+
+    if (isLoading) {
+        toEachRoute(
+            {
+                id: 0,
+                quote: 'Loading',
+                author: 'Loading',
+            },
+            {
+                id: 0,
+                fact: 'Loading',
+            }
+        )
+    }
+
+    if (error) {
+        toEachRoute(INITIAL_QUOTE, INITIAL_FACT)
+    }
 
     return data
 }
