9848aa2dfee0837b250f2908d37c68c5dee6ab8f
Denis <bronzovdenis@bk.ru>
2023-08-05 18:18:22 +0300

    FM-22: add metadata to pages

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -1,5 +1,10 @@
 import { ABOUT_PAGE_CONTENT } from '@/utils/pageContent'
 import PageContent from '@/modules/PageContent'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'FxMath - About us',
+}
 
 const AboutPage = () => {
     return <PageContent {...ABOUT_PAGE_CONTENT} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import HomeContent from '@/modules/HomeContent'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'FxMath - endless math',
+    description: 'Home page FxMath - website with any endless exercises',
+}
 
 const HomePage = () => {
     return <HomeContent />
