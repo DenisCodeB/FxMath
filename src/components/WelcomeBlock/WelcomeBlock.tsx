d16c3921b7f9e88e3ae4cb04fac0651e8577365e
Denis <bronzovdenis@bk.ru>
2023-08-05 18:26:07 +0300

    FM-22: WelcomeBlock transition appearing, Steps fix background switching color

--- src/components/WelcomeBlock/WelcomeBlock.tsx
+++ src/components/WelcomeBlock/WelcomeBlock.tsx
@@ -1,21 +1,34 @@
+'use client'
+
+import c from 'classnames'
 import FaceImages from './components/FaceImages/FaceImages'
 import ScrollDown from './components/ScrollDown/ScrollDown'
 import BackRounds from './components/BackRounds/BackRounds'
 import Title from './components/Title/Title'
 import style from './WelcomeBlock.module.scss'
+import { InView } from 'react-intersection-observer'
 
 const WelcomeBlock = () => {
     return (
-        <section className={style['welcome-block']}>
-            <div className={style['welcome-block__title-wrapper']}>
-                <Title />
-            </div>
-            <div className={style['welcome-block__images']}>
-                <FaceImages />
-                <BackRounds />
-            </div>
-            <ScrollDown />
-        </section>
+        <InView threshold={0}>
+            {({ inView, ref }) => (
+                <section
+                    ref={ref}
+                    className={c(style['welcome-block'], {
+                        [style['welcome-block_hide']]: !inView,
+                    })}
+                >
+                    <div className={style['welcome-block__title-wrapper']}>
+                        <Title />
+                    </div>
+                    <div className={style['welcome-block__images']}>
+                        <FaceImages />
+                        <BackRounds />
+                    </div>
+                    <ScrollDown />
+                </section>
+            )}
+        </InView>
     )
 }
 
