ee2eb6e4b552b5960e4a47e7b4897143c475aa68
Denis <bronzovdenis@bk.ru>
2023-08-05 11:27:50 +0300

    FM-27: add FaceEmoji rotating animation when mouse move

--- src/ui/FaceImage/FaceImage.tsx
+++ src/ui/FaceImage/FaceImage.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import Image, { StaticImageData } from 'next/image'
+import { useEffect, useRef } from 'react'
 import c from 'classnames'
 import { classType } from '@/components/WelcomeBlock/components/FaceImages/helpers/classType'
 import style from './FaceImage.module.scss'
+import { animateMouseMove, removeListeners } from './utils/animateMouseMove'
 
 interface IFaceImage {
     url: StaticImageData
@@ -9,8 +13,19 @@ interface IFaceImage {
 }
 
 const FaceImage = ({ url, className }: IFaceImage) => {
+    const ref = useRef<HTMLImageElement>(null)
+    useEffect(() => {
+        const elem = ref.current as HTMLImageElement
+        animateMouseMove(elem)
+
+        return () => {
+            removeListeners(elem)
+        }
+    }, [])
+
     return (
         <Image
+            ref={ref}
             className={c(style['face-img'], style[className])}
             src={url}
             alt=''
