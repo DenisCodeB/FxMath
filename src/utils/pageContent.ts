5d3abfb2944fe4ea23cfa427287e04e78eb0b7e8
Denis <bronzovdenis59@gmail.com>
2024-06-09 19:12:14 +0300

    FM-30: add updated mock data for marketing pages, update its styles

--- src/utils/pageContent.ts
+++ src/utils/pageContent.ts
@@ -5,17 +5,28 @@ import FaceImage6 from '../../public/assets/images/Image6.jpg'
 export interface IPageContent {
     title: string
     text: string
-    img: StaticImageData
+    img?: StaticImageData
 }
 
+// 'About us' page mock data
 export const ABOUT_PAGE_CONTENT: IPageContent = {
-    title: '🪄 About',
-    text: 'About page content',
+    title: '🎀 About us',
+    text: "Hey, I'm front-end developer. And it's a pet-project, where you can solve unlimited, endless, \
+and infinite equations. Let's practice with us right now.\nBy the way, there will be even more features in the future. \
+My project uses tech stack like React, NextJS, Typescript and others.\nThanks for your user experience!",
     img: FaceImage5,
 }
 
-export const CONTACTS_PAGE_CONTENT = {
-    title: '📱 Contacts',
-    text: 'Contacts page content',
+// 'Contacts' page mock data
+export const CONTACTS_PAGE_CONTENT: IPageContent = {
+    title: '🎙️ Contacts',
+    text: "Hi, I'm developer of this study project. You can contact me on GitHub. I'm open to any cooperations. \
+Glad to work with you, and happy to hear your suggestions.",
     img: FaceImage6,
+}
+
+// 'Not Found' page mock data
+export const NOT_FOUND_PAGE_CONTENT: IPageContent = {
+    title: '🧐 404',
+    text: 'WoW. Page not found :/',
 }
