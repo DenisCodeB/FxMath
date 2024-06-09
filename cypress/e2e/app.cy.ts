fb4c7c8997b10c156905e41258b120ea5a5f57e8
Denis <bronzovdenis59@gmail.com>
2024-06-09 04:23:36 +0300

    FM-29: update jest unit tests, add e2e cypress tests

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,15 @@
+describe('Navigation', () => {
+    it('should navigate to the "About page"', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('a[href*="about"]').click()
+        cy.url().should('include', '/about')
+        cy.get('h2').contains(/About/)
+    })
+
+    it('should navigate to the "Contacts page"', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('a[href*="contacts"]').click()
+        cy.url().should('include', '/contacts')
+        cy.get('h2').contains(/Contacts/)
+    })
+})
